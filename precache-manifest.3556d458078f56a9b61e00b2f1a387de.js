4c6cb07ca4021781fae3857147d9afa0c5925f69
Rodrigo Fessia <rodrigo_fessia@hotmail.com>
2020-10-10 01:14:11 +1300

    Updates

---
+++ precache-manifest.3556d458078f56a9b61e00b2f1a387de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "394ba77047e723ea22939fda2b32cafd",
+    "url": "/rfessia/Monster-Rolodex/index.html"
+  },
+  {
+    "revision": "96d4c86a71ec686789f4",
+    "url": "/rfessia/Monster-Rolodex/static/css/main.62118b21.chunk.css"
+  },
+  {
+    "revision": "c7d7319a71b6cb93be4e",
+    "url": "/rfessia/Monster-Rolodex/static/js/2.73d70fc7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rfessia/Monster-Rolodex/static/js/2.73d70fc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96d4c86a71ec686789f4",
+    "url": "/rfessia/Monster-Rolodex/static/js/main.e40bbb7c.chunk.js"
+  },
+  {
+    "revision": "837110eaf7f0dcdd06e5",
+    "url": "/rfessia/Monster-Rolodex/static/js/runtime-main.b97d3a2d.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/rfessia/Monster-Rolodex/precache-manifest.3556d458078f56a9b61e00b2f1a387de.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rfessia/Monster-Rolodex/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
